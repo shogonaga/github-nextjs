837027a485a58c5bde1aa18fe99b38ac6cfe5463
CryptoNeko (個人投機家) <po@shogo-naganuma.com>
2023-02-05 09:50:22 +0900

    added func-B1

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Func-3 Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Func-B1 Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
         <p className={styles.description}>
